97a59d37e1c79d1b7c7d3f6ba02790a919bfc669
Sean Lennaerts <seanlenny@gmail.com>
2020-05-26 23:20:48 -0700

    tabs to spaces

--- modmain.lua
+++ modmain.lua
@@ -1,15 +1,15 @@
 function inventorypostinit(Inventory,inst)
-	Inventory.OriginalDropEverything = Inventory.DropEverything
-	
-	function Inventory:DropEverything(ondeath, keepequip)
-		if inst:HasTag("player") then
-			-- do nothing
-			print("-- my mod: has tag 'player' don't drop anything!")
-		else
-			print("-- my mod: not player calling original drop everything function")
-			return Inventory:OriginalDropEverything(ondeath, keepequip)
-		end
-	end
+  Inventory.OriginalDropEverything = Inventory.DropEverything
+  
+  function Inventory:DropEverything(ondeath, keepequip)
+    if inst:HasTag("player") then
+      -- do nothing
+      print("-- my mod: has tag 'player' don't drop anything!")
+    else
+      print("-- my mod: not player calling original drop everything function")
+      return Inventory:OriginalDropEverything(ondeath, keepequip)
+    end
+  end
 end
 
 AddComponentPostInit("inventory", inventorypostinit)

6b0eca5fac033d03cbc1469ab85492f53f6b16db
Sean Lennaerts <seanlenny@gmail.com>
2020-05-26 23:22:01 -0700

    removed prints from modmain.lua

--- modmain.lua
+++ modmain.lua
@@ -4,9 +4,7 @@ function inventorypostinit(Inventory,inst)
   function Inventory:DropEverything(ondeath, keepequip)
     if inst:HasTag("player") then
       -- do nothing
-      print("-- my mod: has tag 'player' don't drop anything!")
     else
-      print("-- my mod: not player calling original drop everything function")
       return Inventory:OriginalDropEverything(ondeath, keepequip)
     end
   end
